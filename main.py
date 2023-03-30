77bd25118a57191f711e80737a16ac96234f4b4d
BillStephens2022 <two4onebill@yahoo.com>
2023-03-30 13:01:54 -0400

    write missed states to csv file

--- main.py
+++ main.py
@@ -12,8 +12,19 @@ all_states = data.state.to_list()
 guessed_states = []
 
 
+
 while len(guessed_states) < 50:
-    answer_state = screen.textinput(title=f"{len(guessed_states)} / 50 States Correct", prompt="What's another state's name?")
+    # .title() converts the user's answer to Title case.
+    answer_state = screen.textinput(title=f"{len(guessed_states)} / 50 States Correct", 
+                                    prompt="What's another state's name?").title()
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
     if answer_state in all_states:
         guessed_states.append(answer_state)
         t = turtle.Turtle()
@@ -22,8 +33,6 @@ while len(guessed_states) < 50:
         state_data = data[data.state == answer_state]
         t.goto(int(state_data.x), int(state_data.y))
         t.write(answer_state)
-
-        
 
 
 screen.exitonclick()
